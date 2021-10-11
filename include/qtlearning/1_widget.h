1dc01ad523a6f535f290665c50a316f4d2a223f2
xhguleixin123 <18017158729@163.com>
2021-10-11 22:06:32 +0800

    ✨ feat: model/view模型案例些许

--- include/qtlearning/1_widget.h
+++ include/qtlearning/1_widget.h
@@ -56,3 +56,107 @@ namespace glx0103
     return app.exec();
     }
 }
+
+// Model/View模型简单案例
+namespace glx0104
+{
+    int main(int argc, char *argv[])
+    {
+    QApplication app(argc, argv);
+    
+    // 创建一个滑块控件（如果只有一个控件就是默认顶级）
+    QSplitter* splitter = new QSplitter;
+
+    // 创建一个Model
+    QFileSystemModel* model = new QFileSystemModel;
+    model->setRootPath(QDir::currentPath());
+
+    // 创建两个View
+    QTreeView* tree = new QTreeView(splitter);
+    tree->setModel(model);
+    tree->setRootIndex(model->index(QDir::currentPath()));
+
+    QListView* list = new QListView(splitter);
+    list->setModel(model);
+    list->setRootIndex(model->index(QDir::currentPath()));
+
+    splitter->setWindowTitle("两个View使用同一个Model");
+    splitter->show();
+
+    return app.exec();
+    }
+}
+
+// 使用Model
+namespace glx0105
+{
+    int main(int argc, char *argv[])
+    {
+    QApplication app(argc, argv);
+    
+    QStringList numbers;
+    numbers << "One" << "Two" << "Three" << "Four" << "Five";
+
+    QAbstractItemModel *model = new QStringListModel(numbers);
+
+    QListView* view = new QListView();
+    view->setModel(model);
+
+    view->show();
+    return app.exec();
+
+    }
+}
+
+// 多个View使用同一个Model
+namespace glx0106
+{
+    int main(int argc, char *argv[])
+    {
+    QApplication app(argc, argv);
+    
+    QStringList numbers;
+    numbers << "One" << "Two" << "Three" << "Four" << "Five";
+
+    QAbstractItemModel *model = new QStringListModel(numbers);
+
+    QListView* firstView = new QListView();
+    QListView* secondView = new QListView();
+    firstView->setModel(model);
+    secondView->setModel(model);
+
+    firstView->show();
+    secondView->show();
+    return app.exec();
+    
+    }
+}
+// 多个View使用同一个SelectModel
+namespace glx0107
+{
+    int main(int argc, char *argv[])
+    {
+    QApplication app(argc, argv);
+    
+    // 创建一个滑块控件（如果只有一个控件就是默认顶级）
+    QSplitter* splitter = new QSplitter;
+
+    // 创建一个Model
+    QFileSystemModel* model = new QFileSystemModel;
+    model->setRootPath(QDir::currentPath());
+
+    // 创建两个View
+    QTreeView* tree = new QTreeView(splitter);
+    tree->setModel(model);
+    tree->setRootIndex(model->index(QDir::currentPath()));
+
+    QListView* list = new QListView(splitter);
+    list->setModel(model);
+    list->setRootIndex(model->index(QDir::currentPath()));
+    list->setSelectionModel(tree->selectionModel());
+    splitter->setWindowTitle("两个View使用同一个Model");
+    splitter->show();
+
+    return app.exec();
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,37 @@
 int main(int argc, char *argv[])
 {
     int no = 1;
-    std::cout << "11、初始化一个窗口" << std::endl;
-    std::cout << "12、添加子组件" << std::endl;
-    std::cout << "13、设置布局" << std::endl;
+    std::cout << "101、初始化一个窗口" << std::endl;
+    std::cout << "102、添加子组件" << std::endl;
+    std::cout << "103、设置布局" << std::endl;
+    std::cout << "104、Model/View模型案例" << std::endl;
+    std::cout << "105、简单实用Model" << std::endl;
+    std::cout << "106、多个View使用同一个Model" << std::endl;
+    std::cout << "107、多个View使用同一个SelectModel" << std::endl;
 
     std::cin >> no;
     switch (no)
     {
-    case 11:
+    case 101:
         glx0101::main(argc, argv);
         break;
-    case 12:
+    case 102:
         glx0102::main(argc, argv);
         break;
-    case 13:
+    case 103:
         glx0103::main(argc, argv);
+        break;
+    case 104:
+        glx0104::main(argc, argv);
+        break;
+    case 105:
+        glx0105::main(argc, argv);
+        break;
+    case 106:
+        glx0106::main(argc, argv);
+        break;
+    case 107:
+        glx0107::main(argc, argv);
         break;
     default:
         break;
