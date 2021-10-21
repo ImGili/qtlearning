bf03caf776de7ff064e1600ff9272939cc2aeb97
xhguleixin123 <18017158729@163.com>
2021-10-21 19:42:20 +0800

    ✨ feat: 临时保存

--- examples/learnQTWidget/t1/testwindow.cpp
+++ examples/learnQTWidget/t1/testwindow.cpp
@@ -1,5 +1,10 @@
 #include"testwindow.h"
 #include "ui_testwindow.h"
+
+#include<opencv2/core.hpp>
+#include<opencv2/imgcodecs.hpp>
+#include<opencv2/highgui.hpp>
+using namespace cv;
 #include <iostream>
 
 testWindow::testWindow(QWidget *parent)
@@ -7,7 +12,18 @@ testWindow::testWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    // connect(ui->pushButton, &QPushButton::pressed, this, &MainWindow::on_push_button_clicked);
+    connect(ui->pushButton, &QPushButton::pressed, this, &testWindow::on_push_button_clicked);
+    std::string image_path = samples::findFile("/Users/gulei/Desktop/learnsomething/opencv_learning/resources/imgs/lena.jpg");
+    Mat img = imread(image_path, IMREAD_COLOR);
+    if (img.empty())
+    {
+        std::cout << "Could not read image :" << image_path << std::endl;
+        return ;
+    }
+    // imshow("Display image", img);
+    QImage qimg(img.data, img.cols, img.rows, QImage::Format::Format_BGR888);
+    ui->label->setPixmap(QPixmap::fromImage(qimg));
+    // int k = waitKey(0);
 }
 
 testWindow::~testWindow()
@@ -17,5 +33,9 @@ testWindow::~testWindow()
 
 void testWindow::on_push_button_clicked()
 {
+    static bool isLabelvisible = true;
     std::cout << "button clicked" << std::endl;
+    isLabelvisible = !isLabelvisible;
+
+    ui->label->setVisible(isLabelvisible);
 }
